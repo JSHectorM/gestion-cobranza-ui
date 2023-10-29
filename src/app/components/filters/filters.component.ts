7be15478957631b74ea397a0ef5362e94778b8e5
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-10-29 12:08:09 -0600

    add conect filtro de oficina

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogosService} from "../../services/catalogos.service";
+import {Oficina} from "../../models/oficina";
+import {ResponseAPI} from "../../models/responseAPI";
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss']
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit{
+
+  catOfcinas: Oficina[] = [];
+  selectedOficina !: number ;
+  constructor( private catalogosService : CatalogosService ) { }
+
+  ngOnInit(): void {
+    this.getOficinas()
+  }
+  getOficinas(){
+    this.catalogosService.getcatalogoOficinas()
+        .subscribe({
+          error: error => console.error('Error!', error),
+          next: (response: ResponseAPI) => {
+            this.catOfcinas = response.data;
+            this.selectedOficina = 14;
+            // console.log(this.catOfcinas);
+          }
+        });
+  }
+
 
 }
