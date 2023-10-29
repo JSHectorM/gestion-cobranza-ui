0a2dd628da689ef161252818fa8fd83fc3ca64ce
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-10-29 00:04:56 -0600

    add componetes material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableFianzasComponent } from './components/table-fianzas/table-fianzas.component';
 import { HomeComponent } from './page/home/home.component';
+import {MatSelectModule} from "@angular/material/select";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { FiltersComponent } from './components/filters/filters.component';
+import { CardsComponent } from './components/cards/cards.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   declarations: [
     AppComponent,
     TableFianzasComponent,
-    HomeComponent
+    HomeComponent,
+    FiltersComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
