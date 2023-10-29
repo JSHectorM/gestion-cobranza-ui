744e4b427c31f50368704942e9ae39e960f1673d
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-10-28 23:27:15 -0600

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableFianzasComponent } from './components/table-fianzas/table-fianzas.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableFianzasComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
