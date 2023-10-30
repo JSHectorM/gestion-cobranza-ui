76646cc0df2d298b560cefc82f7a33c1ce13801a
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-10-29 18:06:27 -0600

    add enum fianza

---
+++ src/app/models/fianza.ts
@@ -0,0 +1,11 @@
+export interface Fianza {
+    id_fianza: number,
+    fianza: string,
+    movimiento: string,
+    fiado: string,
+    id_pago_pendiente: number,
+    fecha_movimiento: string,
+    fecha_limite: string,
+    importe: string,
+    id_moneda: number
+}
