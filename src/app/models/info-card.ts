cbccb84a0886c33cc3d32bb4b871ac36e8589cad
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-10-29 18:06:52 -0600

    add interface info-card

---
+++ src/app/models/info-card.ts
@@ -0,0 +1,9 @@
+export interface InfoCard {
+    idCard: number,
+    tipoMoneda: string,
+    cantidadTotal: number,
+    totalEnTiempo: number,
+    porcentajeEnTiempo: number,
+    totalVencido: number,
+    porsentajeVencido: number,
+}
