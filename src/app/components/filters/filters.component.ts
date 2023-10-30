0807619204268aa0eed623b6588b119fbaa5a951
Hector JS <miguel.lin123@gmail.com>
2023-10-30 01:20:09 -0600

    add servicio y logica para btn upload

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,37 +1,86 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {CatalogosService} from "../../services/catalogos.service";
 import {Oficina} from "../../models/oficina";
 import {ResponseAPI} from "../../models/responseAPI";
+import {FianzasService} from "../../services/fianzas.service";
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss']
 })
-export class FiltersComponent implements OnInit{
+export class FiltersComponent implements OnInit {
 
   catOfcinas: Oficina[] = [];
-  selectedOficina !: number ;
+  selectedOficina !: number;
+  nameFile: string  = '';
   @Output() filterOficina = new EventEmitter<number>();
-  constructor( private catalogosService : CatalogosService ) { }
+
+  flagFile = false;
+  docFile !: File
+
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
+  constructor(private catalogosService: CatalogosService,
+              private fianzasService: FianzasService) {
+  }
 
   ngOnInit(): void {
     this.getOficinas()
   }
-  getOficinas(){
+
+  getOficinas() {
     this.catalogosService.getcatalogoOficinas()
-        .subscribe({
-          error: error => console.error('Error!', error),
-          next: (response: ResponseAPI) => {
-            this.catOfcinas = response.data;
-            this.selectedOficina = 14;
-            this.filterOficina.emit(this.selectedOficina);
-            // console.log(this.catOfcinas);
-          }
-        });
+      .subscribe({
+        error: error => console.error('Error!', error),
+        next: (response: ResponseAPI) => {
+          this.catOfcinas = response.data;
+          this.selectedOficina = 14;
+          this.filterOficina.emit(this.selectedOficina);
+          // console.log(this.catOfcinas);
+        }
+      });
   }
 
-    changeOfcina( ){
-        this.filterOficina.emit(this.selectedOficina);
+  changeOfcina() {
+    this.filterOficina.emit(this.selectedOficina);
+  }
+
+  onFileSelected(event: any) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.flagFile = true;
+      this.nameFile = file.name;
+      this.docFile = file;
     }
+  }
+
+  uploadFile() {
+    console.log('uploadFile');
+    this.fianzasService.uploadXLSX(this.docFile, this.selectedOficina.toString())
+      .subscribe(r => {
+        console.log(r)
+        if (r.code === '200 OK'){
+          console.log('Se cargo el archivo correctamente');
+          this.flagFile = false;
+          this.nameFile = '';
+          this.docFile = new File([], '');
+          this.filterOficina.emit(this.selectedOficina);
+          this.cancelUpload()
+        } else {
+          console.error('Ocurrio un error al cargar el archivo');
+        }
+      });
+  }
+
+  cancelUpload() {
+    const fileInput = this.fileInput.nativeElement;
+    fileInput.value = '';
+    this.flagFile = false;
+    this.nameFile = '';
+    this.docFile = new File([], '');
+  }
+
+
 }
